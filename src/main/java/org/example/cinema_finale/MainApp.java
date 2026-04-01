051de8994d97afea850197792e77f47ac1e33826
hoangtungcoder <tongtungboceo@gmail.com>
2026-04-01 12:16:36 +0700

    gắn panel phim vào mainframe thành công

--- src/main/java/org/example/cinema_finale/MainApp.java
+++ src/main/java/org/example/cinema_finale/MainApp.java
@@ -1,5 +1,6 @@
 package org.example.cinema_finale;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import org.example.cinema_finale.view.frame.LoginFrame;
 
 import javax.swing.*;
@@ -7,7 +8,14 @@ import javax.swing.*;
 public class MainApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+
+            try {
+                FlatDarkLaf.setup(); // 🔥 DARK MODE
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             LoginFrame loginFrame = new LoginFrame();
+
             loginFrame.setVisible(true);
         });
     }
